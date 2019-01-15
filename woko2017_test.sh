e2f4c7603e964ce175ae7f6e399fc83f325d426a
Benjamin Champion <benjamin.champion@ligo.org>
2019-01-14 19:13:21 -0500

    Changed WoKo2017 test

--- woko2017_test.sh
+++ woko2017_test.sh
@@ -6,26 +6,26 @@ mkdir em_pe/tests/temp/
 ### parse JSON data
 python em_pe/parser/parse_json.py --f Data/GW170817.json --out em_pe/tests/temp/ --t0 57982.5285231481 \
                                 --b g \
-                                --b y \
-                                --b r \
-                                --b z \
-                                --b K \
-                                --b J \
-                                --b H \
-                                --b i
+                                --b y
+                                #--b r \
+                                #--b z \
+                                #--b K \
+                                #--b J \
+                                #--b H \
+                                #--b i
 
 
 ### Do a normal test in one band with a single model
 python em_pe/generate_posterior_samples.py --dat em_pe/tests/temp/ -v --m woko2017 1 \
                                            --out em_pe/tests/temp/posterior_samples.txt --cutoff 1e-1000 \
                                            --f g.txt \
-                                           --f y.txt \
-                                           --f r.txt \
-                                           --f z.txt \
-                                           --f K.txt \
-                                           --f J.txt \
-                                           --f H.txt \
-                                           --f i.txt
+                                           --f y.txt 
+                                           #--f r.txt \
+                                           #--f z.txt \
+                                           #--f K.txt \
+                                           #--f J.txt \
+                                           #--f H.txt \
+                                           #--f i.txt
 
 ### Make the corner plots
 python em_pe/plot_utils/plot_corner.py --p mej --p vej --p dist --posterior_samples em_pe/tests/temp/posterior_samples.txt \
