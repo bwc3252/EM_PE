59043a150b38fba3567994a44b5bac54973dbe72
Benjamin Champion <benjamin.champion@ligo.org>
2019-02-23 22:20:14 -0500

    Added Python API for JSON parser

--- em_pe/parser/parse_json.py
+++ em_pe/parser/parse_json.py
@@ -4,7 +4,7 @@ Parse JSON
 ----------
 Parse Open Astronomy Catalog (OAC) JSON files and save requested data.
 
-Example::
+CLI Example::
 
     $ python parse_json.py --f data.json --b R --b V --out Data/
 
@@ -43,51 +43,72 @@ def _parse_command_line_args():
     parser.add_argument('--out', help='Directory to save data to')
     parser.add_argument('--maxpts', type=float, default=np.inf, help='Maximum number of points to keep for each band')
     parser.add_argument('--tmax', type=float, default=np.inf, help='Upper bound for time points to keep')
-#    parser.add_argument('--tmin', type=float, default=0, help='Lower bound for time points to keep')
     return parser.parse_args()
 
-def _read_data(args):
-    filename = args.f
-    name = filename.split('/')[-1] # get rid of path except for filename
+def _read_data(t0, file, bands, out, maxpts, tmax):
+    name = file.split('/')[-1] # get rid of path except for filename
     name = name.split('.')[0] # get event name from filename
     ### read in the data
-    with open(filename, "r") as read_file:
+    with open(file, "r") as read_file:
         data = json.load(read_file)[name]['photometry']
     ### create empty data arrays
     data_dict = {}
-    for band in args.b:
+    for band in bands:
         data_dict[band] = np.empty((4, 0))
     for entry in data:
         if 'band' in entry:
             band = entry['band']
             ### check that it's a band we want and that it has an error magnitude
-            if band in args.b and 'e_magnitude' in entry:
+            if band in bands and 'e_magnitude' in entry:
                 ### [time, time error, magnitude, magnitude error]
                 to_append = np.array([[entry['time']], [0], [entry['magnitude']], [entry['e_magnitude']]]).astype(np.float)
-                to_append[0] -= args.t0
-                if to_append[0] < args.tmax:
+                to_append[0] -= t0
+                if to_append[0] < tmax:
                     data_dict[band] = np.append(data_dict[band], to_append, axis=1)
     for band in data_dict:
         data = data_dict[band]
         ### check if we have too much data
-        if data.shape[1] > args.maxpts:
+        if data.shape[1] > maxpts:
             ### basically, generate random indices, take the columns (data points)
             ### specified by those columns, and then sort them based on times
             ### (sorting is not strictly necessary but it seems like a good idea
             ### to keep data ordered)
-            cols = np.random.randint(0, data.shape[1], int(args.maxpts))
+            cols = np.random.randint(0, data.shape[1], int(maxpts))
             data = data[:,cols]
             data = data[:,data[0].argsort()]
             data_dict[band] = data
     return data_dict
 
-def _save_data(args, data_dict):
-    outdir = args.out
+def _save_data(out, data_dict):
     for band in data_dict:
-        filename = outdir + band + '.txt'
+        filename = out + band + '.txt'
         np.savetxt(filename, data_dict[band])
 
-if __name__ == '__main__':
+def parse_json(t0, file, bands, out, maxpts=np.inf, tmax=np.inf):
+    '''
+    Parse JSON file.
+
+    Parameters
+    ----------
+    t0 : int
+        Initial time (t=0) for the event
+    file : string
+        Name of JSON file
+    bands : list
+        List of names of data bands to keep
+    out : string
+        Directory to save data to
+    maxpts : int
+        Maximum number of points to keep for each band
+    tmax : float
+        Upper bound for time points to keep
+    '''
+    data_dict = _read_data(t0, file, bands, out, maxpts, tmax)
+    _save_data(out, data_dict)
+
+def main():
     args = _parse_command_line_args()
-    data_dict = _read_data(args)
-    _save_data(args, data_dict)
+    parse_json(args.t0, args.f, args.b, args.out, args.maxpts, args.tmax)
+
+if __name__ == '__main__':
+    main()
